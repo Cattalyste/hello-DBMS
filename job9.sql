dcba13c95e9bf50aa32e0cbf99d9902824d936e1
Cattalyste <chevalier.saganne@gmail.com>
2024-01-07 23:03:01 +0100

    Add files via upload

---
+++ job9.sql
@@ -0,0 +1,6 @@
+SELECT * FROM countries ORDER BY 'Arable (%)' ASC;
+SELECT * FROM countries ORDER BY 'Literacy (%)' DESC;
+SELECT * FROM countries ORDER BY 'Birthrate' ASC;
+SELECT * FROM countries ORDER BY 'Deathrate' ASC;
+SELECT * FROM countries ORDER BY 'Infant mortality (per 1000 births)' ASC;
+SELECT * FROM countries ORDER BY 'Phones (per 1000)' DESC;
