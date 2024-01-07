f4b8aa0e12938b14008c7febe073763f37f8e8bf
Cattalyste <chevalier.saganne@gmail.com>
2024-01-07 23:57:21 +0100

    Update job9.sql

--- job9.sql
+++ job9.sql
@@ -4,3 +4,5 @@ SELECT * FROM countries ORDER BY 'Birthrate' ASC;
 SELECT * FROM countries ORDER BY 'Deathrate' ASC;
 SELECT * FROM countries ORDER BY 'Infant mortality (per 1000 births)' ASC;
 SELECT * FROM countries ORDER BY 'Phones (per 1000)' DESC;
+#Il est possible de voir que certains pays sont en commun parmis ces différentes 
+#requetes ce qui nous montre la correlation entre certaines variable.
